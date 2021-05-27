f53d04b8c0b6b3afc9fb6ca10a4b3826f5a0f97f
chenrujun-ROC <84781971+chenrujun-ROC@users.noreply.github.com>
2021-05-27 23:21:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from classes import Cog_Extension
+
+class Main(Cog_Extension):
+    
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'{round(self.bot.latency*1000)}(ms)')
+    
+    @commands.command()
+    async def hi(self, ctx):
+        await ctx.send('ABCD')
+    
+
+def setup(bot):
+    bot.add_cog(Main(bot))
+
+
+   
